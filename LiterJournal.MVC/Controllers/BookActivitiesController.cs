aec9dded9d11350224b0418f7651133c5f907d0e
Aaron Villalobos <aaron.villalobos.arguedas@est.una.ac.cr>
2025-09-24 00:15:31 -0600

    [LJ001] Base Controllers and Views

---
+++ LiterJournal.MVC/Controllers/BookActivitiesController.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LiterJournal.MVC.Data;
+using LiterJournal.MVC.Models;
+
+namespace LiterJournal.MVC.Controllers
+{
+    public class BookActivitiesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookActivitiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BookActivities
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.BookActivities.Include(b => b.UserBook);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: BookActivities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookActivity = await _context.BookActivities
+                .Include(b => b.UserBook)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bookActivity == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookActivity);
+        }
+
+        // GET: BookActivities/Create
+        public IActionResult Create()
+        {
+            ViewData["UserBookId"] = new SelectList(_context.UserBooks, "Id", "Id");
+            return View();
+        }
+
+        // POST: BookActivities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserBookId,ActivityDate,ActivityType,Title,Content,Experience")] BookActivity bookActivity)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bookActivity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserBookId"] = new SelectList(_context.UserBooks, "Id", "Id", bookActivity.UserBookId);
+            return View(bookActivity);
+        }
+
+        // GET: BookActivities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookActivity = await _context.BookActivities.FindAsync(id);
+            if (bookActivity == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserBookId"] = new SelectList(_context.UserBooks, "Id", "Id", bookActivity.UserBookId);
+            return View(bookActivity);
+        }
+
+        // POST: BookActivities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserBookId,ActivityDate,ActivityType,Title,Content,Experience")] BookActivity bookActivity)
+        {
+            if (id != bookActivity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bookActivity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookActivityExists(bookActivity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserBookId"] = new SelectList(_context.UserBooks, "Id", "Id", bookActivity.UserBookId);
+            return View(bookActivity);
+        }
+
+        // GET: BookActivities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bookActivity = await _context.BookActivities
+                .Include(b => b.UserBook)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bookActivity == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookActivity);
+        }
+
+        // POST: BookActivities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bookActivity = await _context.BookActivities.FindAsync(id);
+            if (bookActivity != null)
+            {
+                _context.BookActivities.Remove(bookActivity);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookActivityExists(int id)
+        {
+            return _context.BookActivities.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ LiterJournal.MVC/Controllers/UserBooksController.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LiterJournal.MVC.Data;
+using LiterJournal.MVC.Models;
+
+namespace LiterJournal.MVC.Controllers
+{
+    public class UserBooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserBooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserBooks
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.UserBooks.Include(u => u.Book).Include(u => u.UserProfile);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: UserBooks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userBook = await _context.UserBooks
+                .Include(u => u.Book)
+                .Include(u => u.UserProfile)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userBook == null)
+            {
+                return NotFound();
+            }
+
+            return View(userBook);
+        }
+
+        // GET: UserBooks/Create
+        public IActionResult Create()
+        {
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "Author");
+            ViewData["UserProfileId"] = new SelectList(_context.UserProfiles, "Id", "UserId");
+            return View();
+        }
+
+        // POST: UserBooks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserProfileId,BookId,AddedDate,Status,Rating,Review")] UserBook userBook)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userBook);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "Author", userBook.BookId);
+            ViewData["UserProfileId"] = new SelectList(_context.UserProfiles, "Id", "UserId", userBook.UserProfileId);
+            return View(userBook);
+        }
+
+        // GET: UserBooks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userBook = await _context.UserBooks.FindAsync(id);
+            if (userBook == null)
+            {
+                return NotFound();
+            }
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "Author", userBook.BookId);
+            ViewData["UserProfileId"] = new SelectList(_context.UserProfiles, "Id", "UserId", userBook.UserProfileId);
+            return View(userBook);
+        }
+
+        // POST: UserBooks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserProfileId,BookId,AddedDate,Status,Rating,Review")] UserBook userBook)
+        {
+            if (id != userBook.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userBook);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserBookExists(userBook.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "Author", userBook.BookId);
+            ViewData["UserProfileId"] = new SelectList(_context.UserProfiles, "Id", "UserId", userBook.UserProfileId);
+            return View(userBook);
+        }
+
+        // GET: UserBooks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userBook = await _context.UserBooks
+                .Include(u => u.Book)
+                .Include(u => u.UserProfile)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userBook == null)
+            {
+                return NotFound();
+            }
+
+            return View(userBook);
+        }
+
+        // POST: UserBooks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var userBook = await _context.UserBooks.FindAsync(id);
+            if (userBook != null)
+            {
+                _context.UserBooks.Remove(userBook);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserBookExists(int id)
+        {
+            return _context.UserBooks.Any(e => e.Id == id);
+        }
+    }
+}
